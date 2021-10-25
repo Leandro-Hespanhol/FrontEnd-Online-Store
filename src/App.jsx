54e95098a2c1ffb0b8e424713caa70aa208facc5
Arthur Lisboa <lisboaarthur@hotmail.com>
2021-10-25 16:35:28 -0300

    grupo-16-requisito-03

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
+import Cart from './pages/Cart';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
