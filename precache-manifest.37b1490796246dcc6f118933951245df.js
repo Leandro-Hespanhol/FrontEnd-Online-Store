84934f0a4c043412aaec5b63361004ec889fb462
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-02-23 13:41:38 -0300

    Updates

---
+++ precache-manifest.37b1490796246dcc6f118933951245df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e82d5431bada2858cb86424c8cc0b92",
+    "url": "/Frontend-Online-Store-React/index.html"
+  },
+  {
+    "revision": "3b539701986cb558aa9b",
+    "url": "/Frontend-Online-Store-React/static/css/main.ee1e6c61.chunk.css"
+  },
+  {
+    "revision": "25bcf5ce0d29394ab200",
+    "url": "/Frontend-Online-Store-React/static/js/2.3a01d10a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Frontend-Online-Store-React/static/js/2.3a01d10a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b539701986cb558aa9b",
+    "url": "/Frontend-Online-Store-React/static/js/main.aa5931be.chunk.js"
+  },
+  {
+    "revision": "d447d01028d99def4be7",
+    "url": "/Frontend-Online-Store-React/static/js/runtime-main.ce1e8882.js"
+  }
+]);
